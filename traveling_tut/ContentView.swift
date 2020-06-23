2ccb070e86ad60991597e7238f400ab8ad153f45
Ivan Oshchepkov <oshchepkov.ivan@gmail.com>
2020-06-23 22:06:44 +0300

    Initial Commit

---
+++ traveling_tut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  traveling_tut
+//
+//  Created by  Иван Ощепков on 23.06.2020.
+//  Copyright © 2020  Иван Ощепков. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
